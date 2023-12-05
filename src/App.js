6374eeb4ce1f01061bd4ec3cf8cb4d7cda5bb993
Jun wang <wang.jun6@northeastern.edu>
2023-12-05 11:20:16 -0800

    working mouse control

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import ParticleControls from './particleControls';
-import { scene } from './Scene';
-import {EARTH_GRAVITY}  from "./Constants";
-import { setupCanvas } from "./Canvas";  
+import React, { useState, useEffect, useCallback } from "react";
+import ParticleControls from "./particleControls";
+import { scene } from "./Scene";
+import { EARTH_GRAVITY } from "./Constants";
+import { canvas, setupCanvas } from "./Canvas";
+import { cScale } from "./Constants";
 
-function App() {
+export default function App() {
   const [showParticles, setShowParticles] = useState(true);
   const [showGrid, setShowGrid] = useState(true);
   const [compensateDrift, setCompensateDrift] = useState(true);
   const [separateParticles, setSeparateParticles] = useState(true);
   const [paused, setPaused] = useState(true);
-  const [gravity, setGravity] = useState(EARTH_GRAVITY); 
+  const [gravity, setGravity] = useState(EARTH_GRAVITY);
   const [flipRatio, setFlipRatio] = useState(0.9);
+  const [mouseDown, setMouseDown] = useState(false);
 
- 
+  // Control handlers
   const handlePausedChange = () => {
     setPaused(!paused);
   };
@@ -43,13 +45,100 @@ function App() {
     const newGravity = event.target.value;
     setGravity(newGravity);
   };
-  //only run once when the component mounts
+
+  // Define event handler functions with useCallback
+  const startDrag = useCallback((x, y) => {
+    let bounds = canvas.getBoundingClientRect();
+
+    let mx = x - bounds.left - canvas.clientLeft;
+    let my = y - bounds.top - canvas.clientTop;
+    setMouseDown(true);
+
+    x = mx / cScale;
+    y = (canvas.height - my) / cScale;
+
+    scene.setObstacle(x, y, true);
+    scene.paused = false;
+  }, []);
+
+  const drag = useCallback(
+    (x, y) => {
+      if (mouseDown) {
+        let bounds = canvas.getBoundingClientRect();
+        let mx = x - bounds.left - canvas.clientLeft;
+        let my = y - bounds.top - canvas.clientTop;
+        x = mx / cScale;
+        y = (canvas.height - my) / cScale;
+        scene.setObstacle(x, y, false);
+      }
+    },
+    [mouseDown]
+  );
+
+  const endDrag = useCallback(() => {
+    setMouseDown(false);
+    scene.obstacleVelX = 0.0;
+    scene.obstacleVelY = 0.0;
+  }, []);
+
+  const handleMouseDown = useCallback(
+    (event) => {
+      startDrag(event.clientX, event.clientY);
+    },
+    [startDrag]
+  );
+
+  const handleMouseUp = useCallback(() => {
+    endDrag();
+  }, [endDrag]);
+
+  const handleMouseMove = useCallback(
+    (event) => {
+      drag(event.clientX, event.clientY);
+    },
+    [drag]
+  );
+
+  const handleTouchStart = useCallback(
+    (event) => {
+      startDrag(event.touches[0].clientX, event.touches[0].clientY);
+    },
+    [startDrag]
+  );
+
+  const handleTouchEnd = useCallback(() => {
+    endDrag();
+  }, [endDrag]);
+
+  const handleTouchMove = useCallback(
+    (event) => {
+      event.preventDefault();
+      event.stopImmediatePropagation();
+      drag(event.touches[0].clientX, event.touches[0].clientY);
+    },
+    [drag]
+  );
+
+  const handleKeyDown = useCallback((event) => {
+    switch (event.key) {
+      case "p":
+        setPaused(true);
+        break;
+      case "s":
+        setPaused(false);
+        break;
+      default:
+        break;
+    }
+  }, []);
+
+  // useEffect for setting up canvas and scene run once on mount
   useEffect(() => {
     setupCanvas();
     scene.setupScene();
-  }, []); 
+  }, []);
 
-  //run every time the state changes
+  // useEffect for updating scene on state changes
   useEffect(() => {
     scene.gravity = gravity;
     scene.flipRatio = flipRatio;
@@ -59,34 +148,69 @@ function App() {
     scene.separateParticles = separateParticles;
     scene.paused = paused;
     scene.update();
-  }, [showParticles,showGrid, compensateDrift,separateParticles,paused,flipRatio, gravity]);
+  }, [
+    showParticles,
+    showGrid,
+    compensateDrift,
+    separateParticles,
+    paused,
+    flipRatio,
+    gravity,
+  ]);
+
+  // useEffect for setting up and cleaning up event listeners
+  useEffect(() => {
+    canvas.addEventListener("mousedown", handleMouseDown);
+    canvas.addEventListener("mouseup", handleMouseUp);
+    canvas.addEventListener("mousemove", handleMouseMove);
+    canvas.addEventListener("touchstart", handleTouchStart);
+    canvas.addEventListener("touchend", handleTouchEnd);
+    canvas.addEventListener("touchmove", handleTouchMove, { passive: false });
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      canvas.removeEventListener("mousedown", handleMouseDown);
+      canvas.removeEventListener("mouseup", handleMouseUp);
+      canvas.removeEventListener("mousemove", handleMouseMove);
+      canvas.removeEventListener("touchstart", handleTouchStart);
+      canvas.removeEventListener("touchend", handleTouchEnd);
+      canvas.removeEventListener("touchmove", handleTouchMove);
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [
+    handleMouseDown,
+    handleMouseUp,
+    handleMouseMove,
+    handleTouchStart,
+    handleTouchEnd,
+    handleTouchMove,
+    handleKeyDown,
+  ]);
 
   return (
     <div className="App">
       <header className="App-header">
-      <h3>Fluid Simulator</h3>
+        <h3>Fluid Simulator</h3>
       </header>
       <body className="App-body">
-        <ParticleControls 
-        showParticles={showParticles}
-        showGrid={showGrid}
-        compensateDrift={compensateDrift}
-        separateParticles={separateParticles}
-        paused={paused}
-        gravity={gravity}
-        flipRatio={flipRatio}
-        onPausedChange={handlePausedChange}
-        onShowParticlesChange={handleShowParticlesChange}
-        onShowGridChange={handleShowGridChange}
-        onCompensateDriftChange={handleCompensateDriftChange}
-        onSeparateParticlesChange={handleSeparateParticlesChange}
-        onSliderChange={handleSliderChange}
-        onGravityChange={handleGravityChange}
+        <ParticleControls
+          showParticles={showParticles}
+          showGrid={showGrid}
+          compensateDrift={compensateDrift}
+          separateParticles={separateParticles}
+          paused={paused}
+          gravity={gravity}
+          flipRatio={flipRatio}
+          onPausedChange={handlePausedChange}
+          onShowParticlesChange={handleShowParticlesChange}
+          onShowGridChange={handleShowGridChange}
+          onCompensateDriftChange={handleCompensateDriftChange}
+          onSeparateParticlesChange={handleSeparateParticlesChange}
+          onSliderChange={handleSliderChange}
+          onGravityChange={handleGravityChange}
         />
-        <canvas id="myCanvas"/>
+        <canvas id="myCanvas" />
       </body>
     </div>
-  )
+  );
 }
-
-export default App;
--- src/particleControls.js
+++ src/particleControls.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import {EARTH_GRAVITY}  from "./Constants";
 
-// ParticleControls.js
+
 export default function ParticleControls({
   showParticles,
   showGrid,
@@ -75,7 +75,7 @@ export default function ParticleControls({
         id="flipRatioSlider"
         min="0"
         max="10"
-        value={flipRatio}
+        value={flipRatio*10}
         className="slider"
         onChange={onSliderChange}
       />
@@ -95,123 +95,3 @@ export default function ParticleControls({
     </div>
   );
 }
-
-
-// export default function ParticleControls() {
-//   const [showParticles, setShowParticles] = useState(true);
-//   const [showGrid, setShowGrid] = useState(true);
-//   const [compensateDrift, setCompensateDrift] = useState(true);
-//   const [separateParticles, setSeparateParticles] = useState(true);
-//   const [paused, setPaused] = useState(true);
-//   const [gravity, setGravity] = useState(EARTH_GRAVITY); 
-//   const [flipRatio, setFlipRatio] = useState(9);
-
-//   const handleShowParticlesChange = () => {
-//     setShowParticles(!showParticles);
-//   };
-
-//   const handleShowGridChange = () => {
-//     setShowGrid(!showGrid);
-//   };
-
-//   const handleCompensateDriftChange = () => {
-//     setCompensateDrift(!compensateDrift);
-//   };
-
-//   const handleSeparateParticlesChange = () => {
-//     setSeparateParticles(!separateParticles);
-//   };
-
-//   const handlePausedChange = () => {
-//     setPaused(!paused);
-//   }
-
-//   const handleSliderChange = (event) => {
-//     const newFlipRatio = 0.1 * event.target.value;
-//     setFlipRatio(newFlipRatio);
-//   };
-
-//   const handleGravityChange = (event) => {
-//     const newGravity = event.target.value;
-//     setGravity(newGravity);
-//   };
-//   const buttonStyle = (value) => {
-//     return {
-//       backgroundColor: value ? "green" : "grey",
-//     };
-//   };
-
-
-//   return (
-//     <div>
-//        <button
-//         type="button"
-//         onClick={handlePausedChange}
-//         value={paused}
-//         style={buttonStyle(paused)}
-//       >
-//        {paused?"Start":"Pause"}
-//       </button>
-//       <button
-//         type="button"
-//         onClick={handleShowParticlesChange}
-//         value={showParticles}
-//         style={buttonStyle(showParticles)}
-//       >
-//         Particles
-//       </button>
-
-//       <button
-//         type="button"
-//         onClick={handleShowGridChange}
-//         value={showGrid}
-//         style={buttonStyle(showGrid)}
-//       >
-//         Grid
-//       </button>
-
-//       <button
-//         type="button"
-//         onClick={handleCompensateDriftChange}
-//         value={compensateDrift}
-//         style={buttonStyle(compensateDrift)}
-//       >
-//         Compensate Drift
-//       </button>
-
-//       <button
-//         type="button"
-//         onClick={handleSeparateParticlesChange}
-//         value={separateParticles}
-//         style={buttonStyle(separateParticles)}
-//       >
-//         Separate Particles
-//       </button>
-//       <label>PIC</label>
-//       <input
-//         type="range"
-//         id="flipRatioSlider"
-//         min="0"
-//         max="10"
-//         value={flipRatio * 10}
-//         className="slider"
-//         onChange={handleSliderChange}
-        
-//       />
-//       <label>FLIP</label>
-
-//       <label>Gravity</label>
-//       <input
-//         type="range"
-//         id="gravitySlider"
-//         min="-20"
-//         max="0"
-//         value={gravity}
-//         className="slider"
-//         onChange={handleGravityChange}
-      
-//       />
-//       <label> {gravity}</label>
-//     </div>
-//   );
-// }
